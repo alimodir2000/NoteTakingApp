54c1e3fd623864838979bd42d936642d41b64929
Ali <ali_modir2000@yahoo.com>
2023-03-05 00:34:44 +0330

    Identity Server Adedd

---
+++ src/IdentityServer/Config.cs
@@ -0,0 +1,51 @@
+﻿using IdentityServer4.Models;
+
+namespace IdentityServer;
+
+public partial class Program
+{
+    public class Config
+    {
+        public static IEnumerable<Client> GetAllApiClients()
+        {
+            return new List<Client>()
+            {
+                new Client()
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "PresentationWebApi" }
+                }
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetAllApiResources()
+        {
+            return new List<ApiResource>()
+            {
+                new ApiResource("Presentation.Web.Api", "Note Taking Web API")
+            };
+        }
+
+        public static IEnumerable<ApiScope> GetAllScopes()
+        {
+           return new List<ApiScope>()
+           {
+               new ApiScope()
+               {
+                   Name = "PresentationWebApi",
+                   Emphasize=true,
+               },
+               new ApiScope
+               {
+                   Name = "Presentation.Web.Api",
+                   Emphasize=true,
+               },
+           };
+        }
+    }
+}
---
+++ src/IdentityServer/Program.cs
@@ -0,0 +1,25 @@
+namespace IdentityServer;
+
+public partial class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        var config = builder.Configuration;
+
+        builder.Services.AddIdentityServer()
+            .AddDeveloperSigningCredential()
+            .AddInMemoryApiScopes(Config.GetAllScopes())
+            .AddInMemoryApiResources(Config.GetAllApiResources())
+            .AddInMemoryClients(Config.GetAllApiClients());
+
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.UseIdentityServer();
+
+        app.Run();
+    }
+}
